1d1434f98dc17110a21882f6ad54f177bbd74063
zaulin <zaulin@yahoo.com>
2022-06-11 12:35:13 +0200

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,22 +1,33 @@
 function clickNomenclator(){
+    document.getElementById("loadingBlack").style.display = "block";
     window.open("nomenclator/index.html","_self")
 }
 
 function clickActes(){
+    document.getElementById("loadingBlack").style.display = "block";
     window.open("actes/index.html","_self")
 }
 
 function clickTrixis(){
+    document.getElementById("loadingBlack").style.display = "block";
     window.open("trixis/index.html","_self")
 }
 
 function clickPermisos(){
+    document.getElementById("loadingBlack").style.display = "block";
     window.open("permisos/index.html","_self")
 }
 
 function clickVTC(){
+    document.getElementById("loadingBlack").style.display = "block";
     window.open("VTC/index.html","_self")
 }
 
+function clickNegra(){
+    document.getElementById("loadingBlack").style.display = "block";
+    window.open("negra/index.html","_self")
+}
+
 function pageonload() {
+    document.getElementById("loadingBlack").style.display = "none";
 }
